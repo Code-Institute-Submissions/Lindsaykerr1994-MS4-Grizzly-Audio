f5aa299fcb0496295841e9925af2fd1846faa209
Lindsaykerr1994 <lindsaykerr1994@gmail.com>
2020-08-01 15:25:16 +0000

    Created packs.html and a majority of structuring

--- packs/views.py
+++ packs/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Pack
 
-# Create your views here.
+
+def all_packs(request):
+    """ Show all audio packs, allow sorting and search queries """
+
+    packs = Pack.objects.all()
+
+    context = {
+        'packs': packs,
+    }
+
+    return render(request, 'packs/packs.html', context)
