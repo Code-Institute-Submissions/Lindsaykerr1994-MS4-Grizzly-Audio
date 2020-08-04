e94ebd4f5d44f7f5cc6584354b49c8a865b810b4
Lindsaykerr1994 <lindsaykerr1994@gmail.com>
2020-08-04 15:36:48 +0000

    Updated carousel links in index.html to redirect to pack_details

--- home/views.py
+++ home/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from packs.models import Pack
 
 # Create your views here.
 
 
 def index(request):
     """ A view to return the website's home page """
-    return render(request, 'home/index.html')
+    best_sellers = Pack.objects.all()
+    new_release = Pack.objects.all()
+    free_packs = Pack.objects.all()
+
+    context = {
+        'best_sellers': best_sellers,
+        'new_releases': new_release,
+        'free_packs': free_packs,
+    }
+    return render(request, 'home/index.html', context)
