914e7af0e7bb18cf3dfe4e1db5554a3e599647aa
Lindsaykerr1994 <lindsaykerr1994@gmail.com>
2020-08-12 08:35:57 +0000

    Installed Stripe, updated JS to handle errors and checkout.views

--- checkout/views.py
+++ checkout/views.py
@@ -1,19 +1,36 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
+from bag.contexts import bag_contents
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There is nothing in your shopping bag right now")
         return redirect(reverse('packs'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['total']
+    """ This value must be an integer for Stripe's use """
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing.')
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51H5Aq1Aweg9QeXppnZhu4gw4h8mdUJRECJuVopDccrF3MFo9GYpgX9HiW5tWbkXQPprs7NCUVWMQbUbUM4eAEf52003cgncuZ4',
-        'client_secret_key': 'test secret key',
+        'stripe_public_key': stripe_public_key,
+        'client_secret_key': intent.client_secret,
     }
     return render(request, template, context)
--- static/js/stripe_elements.js
+++ static/js/stripe_elements.js
@@ -3,9 +3,10 @@
 /* https://stripe.com/docs/stripe-js */
 
 /* Import required keys from document */
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret_key = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+/* These variables are camel case to adher to following code */
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 /* Assign necessary elements */
 var elements = stripe.elements();
 
@@ -31,4 +32,42 @@ var style = {
 var card = elements.create('card', {style: style});
 /* Mount Stripe elements */
 card.mount('#card-element');
+/* Create a form to submit payment */
+var form = document.getElementById('payment-form');
 
+form.addEventListener('submit', function(ev) {
+  ev.preventDefault();
+    /* Disable the card element and submit button to prevent multiple payments */
+    card.update({'disabled': true});
+    $('#submit-button').attr('disabled', true)
+  stripe.confirmCardPayment(clientSecret, {
+    payment_method: {
+      card: card,
+
+    }
+  }).then(function(result) {
+    if (result.error) {
+      // Show error to your customer (e.g., insufficient funds)
+      var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            /* Re-enable the card and submit button to try again */
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+    } else {
+      // The payment has been processed!
+      if (result.paymentIntent.status === 'succeeded') {
+        // Show a success message to your customer
+        // There's a risk of the customer closing the window before callback
+        // execution. Set up a webhook or plugin to listen for the
+        // payment_intent.succeeded event that handles any business critical
+        // post-payment actions.
+        form.submit();
+      }
+    }
+  });
+});
