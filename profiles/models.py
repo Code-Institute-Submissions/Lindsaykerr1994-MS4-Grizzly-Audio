22daf9221b52497fab279632fcd1f33674ee5c38
Lindsaykerr1994 <lindsaykerr1994@gmail.com>
2020-08-13 13:06:15 +0000

    Updated information of profile.views and html as well as allauth templates

--- profiles/models.py
+++ profiles/models.py
@@ -12,14 +12,14 @@ class UserProfile(models.Model):
     default_phone_number = models.CharField(max_length=20, null=True, 
                                             blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True,
-                                               blank=False)
+                                               blank=True)
     default_street_address2 = models.CharField(max_length=80,
                                                null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True,
                                             blank=True)
     default_county = models.CharField(max_length=40, null=True, blank=True)
     default_post_code = models.CharField(max_length=10, null=True, blank=True)
-    default_country = CountryField(blank_label='Country *', null=True,
+    default_country = CountryField(blank_label='Country', null=True,
                                   blank=True)
 
     def __str__(self):
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from .models import UserProfile
+from .forms import UserProfileForm
 
-# Create your views here.
+
+def profile(request):
+    # A view to display the user's profile
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated your information')
+        else:
+            messages.error(request, 'There was a problem updating your\
+                information. Please check your information and try again.')
+    # Import user's information from .forms
+    form = UserProfileForm(instance=profile)
+    # Import user's order for order history
+    orders = profile.orders.all()
+    template = 'profiles/profile.html'
+    context = {
+        'form': form,
+        'orders': orders,
+        'on_profile': True,
+    }
+    return render(request, template, context)
