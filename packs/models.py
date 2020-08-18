08cf0a5a188888638b3d08aa7113117a6d87ac0a
Lindsaykerr1994 <lindsaykerr1994@gmail.com>
2020-08-18 12:48:58 +0000

    Updated publish_date fields in packs, and added icons to footer

--- packs/models.py
+++ packs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 
 class Category(models.Model):
@@ -19,7 +20,7 @@ class Pack(models.Model):
     sku = models.CharField(max_length=254, null=False, blank=False)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    publish_date = models.DateField(auto_now_add=True, null=True)
+    publish_date = models.DateField(auto_now_add=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
     on_sale = models.BooleanField(default=False)
     reduced_price = models.DecimalField(max_digits=6, decimal_places=2,
