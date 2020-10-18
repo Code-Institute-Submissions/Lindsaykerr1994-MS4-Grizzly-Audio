548d656022ead60a15cf75ff89e77521bf3efa57
Lindsaykerr1994 <lindsaykerr1994@gmail.com>
2020-10-18 11:27:29 +0000

    Fixed typo in DATABASES variable

--- grizzly_audio/settings.py
+++ grizzly_audio/settings.py
@@ -117,7 +117,7 @@ WSGI_APPLICATION = 'grizzly_audio.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATAVASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
